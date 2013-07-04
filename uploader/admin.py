7c15105040184f3a5ba616c7bb38ac7a997c1471
Michal Knapik <michael.knapik@stfc.ac.uk>
2013-07-04 10:27:26 +0100

    add basic models, enable django-admin

---
+++ uploader/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from uploader.models import CvmFs, Package
+
+admin.site.register(CvmFs)
+admin.site.register(Package)
--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+# class User(models.Model):
+#     name = models.CharField(max_length=200, null=False)
+#     cert = models.CharField(max_length=1000)
+#
+#     def __unicode__(self):
+#         return 'User[name=' + str(self.name) + ', cert=' + str(self.cert) + ']'
+
+
+class CvmFs(models.Model):
+    mount_point = models.CharField(max_length=2000, null=False, unique=True)
+
+    def __unicode__(self):
+        return self.mount_point
+
+
+class Package(models.Model):
+    fs = models.ForeignKey(CvmFs)
+    file_path = models.FilePathField(path='/', allow_files=True, allow_folders=False, null=True)
+
+    def __unicode__(self):
+        return 'Package[fs=' + str(self.fs) + ', file_path=' + self.file_path + ']'
+
