d314158a2c13980f795fe8801961c7065d32af45
Michal Knapik <michael.knapik@stfc.ac.uk>
2013-07-04 12:01:07 +0100

    custom admin setting

--- uploader/admin.py
+++ uploader/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from uploader.models import CvmFs, Package
 
+
+class PackageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['file_path']}),
+        ('Example Set of Fields', {'fields': ['fs', 'status']}) # 'classes': ['collapse']
+    ]
+    list_display = ('fs', 'file_path', 'status')
+    list_filter = ('fs', 'status')
+
+
 admin.site.register(CvmFs)
-admin.site.register(Package)
+admin.site.register(Package, PackageAdmin)
--- uploader/models.py
+++ uploader/models.py
@@ -11,15 +11,18 @@ from django.db import models
 
 
 class CvmFs(models.Model):
-    mount_point = models.CharField(max_length=2000, null=False, unique=True)
+    mount_point = models.CharField(max_length=2000, null=False, blank=False, unique=True)
 
     def __unicode__(self):
         return self.mount_point
 
 
 class Package(models.Model):
+    STATUSES = [(a, a) for a in ['new', 'uploading', 'uploaded', 'unpacking', 'unpacked', 'cancelled', 'error']]
+    STATUSES_LENGTH = 10 # max(STATUSES, key=len)
     fs = models.ForeignKey(CvmFs)
     file_path = models.FilePathField(path='/', allow_files=True, allow_folders=False, null=True)
+    status = models.CharField(max_length=STATUSES_LENGTH, choices=STATUSES, blank=False, null=False)
 
     def __unicode__(self):
         return 'Package[fs=' + str(self.fs) + ', file_path=' + self.file_path + ']'
