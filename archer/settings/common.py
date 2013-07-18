c5d9dff2cdc21a9b7c9ba1f674cf8b10f798f74d
Michal Knapik <michael.knapik@stfc.ac.uk>
2013-07-18 17:11:37 +0100

    add missing comma

--- archer/settings/common.py
+++ archer/settings/common.py
@@ -78,7 +78,7 @@ STATICFILES_DIRS = (
 # Put strings here, like "/home/html/static" or "C:/www/django/static".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-    '/var/www/t1student0.esc.rl.ac.uk/archer/uploader/static/'
+    '/var/www/t1student0.esc.rl.ac.uk/archer/uploader/static/',
 )
 
 # List of finder classes that know how to find static files in
