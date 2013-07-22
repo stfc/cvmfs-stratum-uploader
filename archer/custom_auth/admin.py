365784ad9f01548be3d6bf2be917640c79b6e13a
Michal Knapik <michael.knapik@stfc.ac.uk>
2013-07-22 12:20:27 +0100

    fix adding user in django-admin

--- archer/custom_auth/admin.py
+++ archer/custom_auth/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils.translation import ugettext_lazy as _
 from django import forms
+from guardian.admin import GuardedModelAdmin
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -25,8 +26,21 @@ class CustomUserCreationForm(UserCreationForm):
                                     'invalid': _("This value may contain only letters, numbers and "
                                                  "@/./+/-/_/=/ // characters.")})
 
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
 
-class CustomUserAdmin(UserAdmin):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+
+class CustomUserAdmin(UserAdmin, GuardedModelAdmin):
     form = CustomUserChangeForm
     add_form = CustomUserCreationForm
 
