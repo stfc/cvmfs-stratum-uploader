484f87dfb3b252f0320a30dd488dcbdfd56b5780
Michal Knapik <michael.knapik@stfc.ac.uk>
2013-07-29 12:29:09 +0100

    remove specific version of psycopg2, use any instead (e.g. provided by OS)

--- archer/settings/prod.py
+++ archer/settings/prod.py
@@ -10,10 +10,10 @@ class Production(Common):
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'archer_prod', # Or path to database file if using sqlite3.
+            'NAME': 'uploader', # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': 'django',
-            'PASSWORD': '234',
+            'PASSWORD': '1password1',
             'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
             'PORT': '', # Set to empty string for default.
         }
