edfdd180284fd2e2862fd45664c84cdc897f0211
Michal Knapik <michael.knapik@stfc.ac.uk>
2013-08-02 12:46:01 +0100

    add setup.py, add LICENCE information

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## Uploader
+
+Provides interface for uploading and distributing software through cvmfs repositories.
+
+Uses Certificate Authentication provided by Apache `httpd` web server (or others).
+
 ## Prerequisites
 
 + **Python** 2.7.3
@@ -184,4 +190,16 @@ The application is developed by [Michał Knapik](http://github.com/mknapik) for 
 
 ## License
 
-???
+    Copyright 2013 Michał Knapik
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+from pip.req import parse_requirements
+import re
+import os
+
+base_name = 'archer'
+v_file = open(os.path.join(os.path.dirname(__file__),
+                           base_name, '__init__.py'))
+print v_file
+VERSION = re.compile(r".*__version__ = '(.*?)'",
+                     re.S).match(v_file.read()).group(1)
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+reqs = parse_requirements('requirements/prod.txt')
+for r in reqs:
+    print r
+setup(
+    name='uploader',
+    version=VERSION,
+    author=u'Michal Knapik',
+    author_email='Michael.Knapik@stfc.ac.uk',
+    package_dir={base_name: base_name},
+    packages=find_packages(), # include all packages under this directory  
+    include_package_data=True,
+    url='git://github.com/stfc/uploader.git',
+    license='Apache 2.0',
+    description='Provides interface for uploading and distributing software through cvmfs repositories.',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    # Adds dependencies    
+    install_requires=[str(ir.req) for ir in reqs],
+)
