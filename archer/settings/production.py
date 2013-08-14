22721b2156ddc23939194ff93dfe6a84d915652d
Michal Knapik <michael.knapik@stfc.ac.uk>
2013-08-14 14:28:10 +0100

    production config in the text file

--- archer/settings/production.py
+++ archer/settings/production.py
@@ -1,4 +1,5 @@
 # noinspection PyUnresolvedReferences
+import re
 from archer.settings.common import Common
 
 
@@ -17,3 +18,60 @@ class Production(Common):
             'PORT': '',  # Set to empty string for default. Not used with sqlite3.
         }
     }
+
+
+def load_cfg():
+    import ConfigParser
+    import os
+
+    def cast(value):
+        if value.isdigit():
+            return int(value)
+        elif value.lower() in ('true', 'false'):
+            return value.lower() == 'true'
+        else:
+            return value
+
+    def set(option, value):
+        print('%s: %s' % (option, value))
+        setattr(Production, option.upper(), value)
+
+    OPTIONS_AVAILABLE = {
+        'path': ['PROJECT_ROOT', 'MEDIA_ROOT', 'STATIC_ROOT'],
+        'url': ['HOSTNAME', 'MEDIA_URL', 'STATIC_URL'],
+        'security': ['SECRET_KEY', 'CSRF_MIDDLEWARE_SECRET', 'ALLOWED_HOSTS'],
+        'debug': ['DEBUG', 'TEMPLATE_DEBUG', 'VIEW_TEST', 'INTERNAL_IPS', 'SKIP_CSRF_MIDDLEWARE'],
+        'misc': None,
+    }
+    LIST_TYPES = (
+        ('debug', 'INTERNAL_IPS',),
+        ('security', 'ALLOWED_HOSTS',),
+    )
+
+    config = ConfigParser.SafeConfigParser()
+    config.read(['/etc/uploader.cfg', os.path.expanduser('~/.uploader.cfg'), './archer/settings/production.cfg'])
+    for section in config.sections():
+        if section == 'database':
+            default_db = {}
+            for option in config.options(section):
+                default_db[option.upper()] = config.get(section, option)
+            set('DATABASE', {'default': default_db})
+        elif section == 'logging':
+            print 'TBD'
+        else:
+            if section not in OPTIONS_AVAILABLE.keys():
+                raise ValueError('Unrecognized section: [%s]. Perhaps, [misc] should be used instead.' % section)
+
+            for option in config.options(section):
+                option = option.upper()
+                if OPTIONS_AVAILABLE[section] is not None and option not in OPTIONS_AVAILABLE[section]:
+                    raise ValueError('Option "%s" is not available for section [%s]' % (option, section))
+                value = config.get(section, option)
+
+                if (section, option,) in LIST_TYPES:
+                    set(option, tuple(re.split(',\s*', value)))
+                else:
+                    set(option, cast(value))
+
+
+load_cfg()
